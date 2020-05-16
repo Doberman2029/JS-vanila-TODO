ab92011d383aa1c655799d6eff41613d67ab484b
Doberman2029 <yakushina20@mail.ru>
2020-05-16 14:16:14 +0300

    Add todoItems to localStorage

--- main.js
+++ main.js
@@ -6,15 +6,16 @@ let html = document.querySelector('html')
 let modal   
 let changeNameBTN = document.querySelector('#changeName')
 let logoutBtn = document.querySelector('#logout')
+let todoArray = []
 
 //  Name Changer and actions with Local storage
 changeNameBTN.addEventListener('click', changeName)
 logoutBtn.addEventListener('click', clearLocalStor)
-
+window.addEventListener('beforeunload', addAllTodoToLocalStorage)
 
 
 function changeName(){
-    let newName = prompt('Введите имя')
+    let newName = prompt('Введите имя').trim()
     if(newName){
     user.innerHTML = `Hello, ${newName}`
     localStorage.setItem('name', newName)
@@ -26,17 +27,24 @@ function changeName(){
 
 function localStor(){
     let userName = localStorage.getItem('name')
+    let todoList = JSON.parse(localStorage.getItem('todoList'))
     if(userName){
         user.innerHTML = `Hello, ${userName}`
     } else {
         user.innerHTML = "Hello, <span id='nameInput'>write your name</span>"
     }
+    if(todoList !== null && todoList.length > 0){
+        todoList.forEach(el => createNewTODOItem(`${el.title}`, `${el.description}`))
+    } else {
+        list.innerHTML = '<span class="emptyList"> Here will be your targets </span>'
+    }
 }
 
-localStor() // Check for name in local storage
+// Check for name and todoItems in local storage
 
 function clearLocalStor(){
     localStorage.clear()
+    todoArray = []
     localStor()
 }
 
@@ -76,10 +84,11 @@ function createNewTODOItem(title = 'Title', description = 'Here can be your text
     if(list.firstChild === document.querySelector('.emptyList')){
        list.innerHTML = ''
     }
+    let elementId = Math.random().toString()
     let newItem = document.createElement('div')
     newItem.classList.add('todo__item')
     newItem.innerHTML = `
-                <span class="todo__item-delete">&#10006;</span>
+                <span class="todo__item-delete" id="${elementId}">&#10006;</span>
                 <div class="todo__title">${title}</div>
                 <hr>
                 <div class="todo__description">${description}</div>
@@ -87,6 +96,11 @@ function createNewTODOItem(title = 'Title', description = 'Here can be your text
     list.append(newItem)
     let crosses = document.querySelectorAll('.todo__item-delete')
     crosses.forEach(el => el.addEventListener('click', deleteThatListItem))
+    todoArray.push({
+        title: `${title}`,
+        description: `${description}`,
+        id: `${elementId}`
+    })
     
 }
 
@@ -103,16 +117,29 @@ function createToDo(){
 }
 
 
-
-
-function deleteThatListItem(element){   
-    element.target.removeEventListener('click', deleteThatListItem)
-    element.path[1].remove()
+function checkForEmptyList(){
     if(list.children.length === 0){
         list.innerHTML = '<span class="emptyList"> Here will be your targets </span>'
     }
 }
 
+function deleteThatListItem(element){   
+    console.log(todoArray)
+    element.target.removeEventListener('click', deleteThatListItem)
+    element.target.parentElement.remove()
+    let targetId = Object.values(element.target.id).join('')
+    let targetIndexInTodoArray = todoArray.findIndex(obj => obj.id === targetId)
+    console.log(targetIndexInTodoArray)
+    todoArray.splice(targetIndexInTodoArray, 1)
+    console.log(todoArray)
+    checkForEmptyList()
+}
 
 
-createNewTODOItem("You're first target", "You need to do something")
+function addAllTodoToLocalStorage(){
+    let todoList = JSON.stringify(todoArray)
+    localStorage.setItem('todoList', todoList)
+}
+
+localStor()
+checkForEmptyList()
